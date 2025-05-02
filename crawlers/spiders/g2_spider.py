5fc14490734691a365a0b164c9c332b9cc48746b
Dipan Ghosh <dipan.ghosh@hotmail.com>
2025-05-03 03:39:47 +0530

    add: created spider for g2

---
+++ crawlers/spiders/g2_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+from datetime import datetime
+from crawlers.items import Item
+
+class G2Spider(scrapy.Spider):
+    name = "g2"
+    allowed_domains = ["g2.com"]
+    start_urls = ["https://www.g2.com/categories"]
+
+    custom_settings = {
+        "PLAYWRIGHT_ENABLED": True,
+        "PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT": 60 * 1000,
+        "PLAYWRIGHT_LAUNCH_OPTIONS": {"headless": True},
+    }
+
+    def parse(self, response):
+        listings = response.css("div.product-list div.product-list-item")
+        for listing in listings:
+            item = Item()
+            item["url"] = response.url
+            item["title"] = listing.css("h3 a::text").get()
+            item["author"] = None
+            item["published"] = None
+            item["tags"] = listing.css("a.tag::text").getall()
+            item["images"] = listing.css("img::attr(src)").getall()
+            item["content"] = listing.css("div.text-truncate::text").getall()
+            item["raw_html"] = listing.get()
+            item["scraped_at"] = datetime.utcnow().isoformat()
+            yield item
