09d94c57c2ba34fa7f2767f40e4babdb03fd502c
Dipan Ghosh <dipan.ghosh@hotmail.com>
2025-05-03 04:20:05 +0530

    refactor: obeys ethical scraping

--- crawlers/settings.py
+++ crawlers/settings.py
@@ -7,6 +7,8 @@ ROBOTSTXT_OBEY = True #obeys robots.txt(ethical scraping)
 DOWNLOAD_DELAY = 10
 AUTOTHROTTLE_ENABLED = True
 
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
+
 DOWNLOAD_HANDLERS = {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
