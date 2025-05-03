042370cffec469185281a90d73d1470ca4900e7e
Dipan Ghosh <dipan.ghosh@hotmail.com>
2025-05-03 11:27:47 +0530

    refactor: Rotating user agentsn & playwright

--- crawlers/settings.py
+++ crawlers/settings.py
@@ -2,19 +2,29 @@ BOT_NAME = "crawlers"
 
 SPIDER_MODULES = ["crawlers.spiders"]
 
-ROBOTSTXT_OBEY = True #obeys robots.txt(ethical scraping)
-
+#obeys robots.txt(ethical scraping)
+ROBOTSTXT_OBEY = True
 DOWNLOAD_DELAY = 10
 AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 10
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_DEBUG = False
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
+
+# Rotating user agents
+DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    "scrapy_user_agents.middlewares.RandomUserAgentMiddleware": 400,
+}
 
 DOWNLOAD_HANDLERS = {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
-
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 PLAYWRIGHT_BROWSER_TYPE = "chromium"
+PLAYWRIGHT_LAUNCH_OPTIONS = {"headless": True}
 
 LOG_LEVEL = "INFO"
