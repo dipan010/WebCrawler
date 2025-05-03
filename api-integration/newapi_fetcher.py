902aef62aac78c2cdc3910e83c08a96b0838d0f9
Dipan Ghosh <dipan.ghosh@hotmail.com>
2025-05-03 17:06:35 +0530

    refactor: modularized article

--- api-integration/newapi_fetcher.py
+++ api-integration/newapi_fetcher.py
@@ -1,51 +1,47 @@
 import requests
-import os
+import os, sys
 from datetime import datetime
-# from crawlers.items import Item
 
-API_KEY = os.getenv("NEWSAPI_KEY")
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from crawlers.items import Item
+
+API_KEY = "0500c2e672464d08b5db8d0348be270d"
 BASE_URL = "https://newsapi.org/v2/everything"
 
-def fetch_news(query="supply chain OR logistics", page_size = 20):
-    # items = Item()
+def fetch_news(query="supply chain OR logistics", page_size=20):
     params = {
-        "q"          : query,
-        "language"  : "en",
-        "pageSize"  : page_size,
-        "apiKey"    : API_KEY,
-        "sortBy"    : "relevancy, publishedAt",
-        "searchIn"  : "title,content",
-        "from"      : "2025-01-01",
-        "to"        : "2025-02-02"
+        "q": query,
+        "language": "en",
+        "pageSize": page_size,
+        "apiKey": API_KEY,
+        "sortBy": "relevancy",
+        "searchIn": "title,content",
+        "from": "2025-01-01",
+        "to": "2025-02-02"
     }
     response = requests.get(BASE_URL, params=params)
     response.raise_for_status()
     data = response.json()
-    print(data)
-    articles = []
-    print("Status Code:", response.status_code)
-    print("Response JSON keys:", data.keys())
-    print("Total Results:", data.get("totalResults"))
 
-    for article in data.get("articles",[]):
-        articles.append({
-            "source"        : article["source"],
-            "title"         : article["title"],
-            "author"        : article["author"],
-            "description"   : article["description"],
-            "url"           : article["url"],
-            "published_at"  : article["publishedAt"],
-            "content"       : article["content"],
-            "raw"           : article,
-            "scraped_at"    : datetime.utcnow().isoformat()
-        })
-    return articles
+    for article in data.get("articles", []):
+        Item(
+            url=article["url"],
+            title=article["title"],
+            author=article["author"],
+            published=article["publishedAt"],
+            content=article["content"],
+            raw_html=str(article),
+            scraped_at=datetime.utcnow().isoformat(),
+            source=article["source"]["name"]
+        )
+    return article
 
 if __name__ == "__main__":
     news = fetch_news()
     if not news:
         print("No articles returned.")
     else:
-        print(f"\n{len(news)} articles fetched:\n")
+        print(f"\n {len(news)} articles fetched:\n")
         for i, art in enumerate(news, 1):
             print(f"{i}. {art['title']} — {art['url']}")
