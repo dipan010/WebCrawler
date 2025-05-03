a3a9dcdb4ca816c60899ad24c6ba8fda2807f571
Dipan Ghosh <dipan.ghosh@hotmail.com>
2025-05-04 04:01:42 +0530

    Raw data preservation & processed segregation

--- processing/utils/uploader.py
+++ processing/utils/uploader.py
@@ -7,6 +7,10 @@ import os
 BUCKET = "bucket-byte8"
 s3_client = get_s3_client()
 
+def _generate_key(prefix: str, source: str) -> str:
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d")
+    return f"{prefix}/{source}/{timestamp}/{uuid.uuid4()}.json"
+
 def hash_article(article: dict) -> str:
     return hashlib.md5(article["url"].encode()).hexdigest()
 
@@ -14,6 +18,15 @@ def upload_article(article: dict, source: str):
     """
     Uploads an article to S3 in either raw/ or processed/ based on `is_raw`.
     """
+    raw_key = _generate_key("raw", source)
+    s3_client.put_object(
+        Bucket=BUCKET,
+        Key=raw_key,
+        Body=json.dumps(article),
+        ContentType="application/json"
+    )
+    print(f"Uploaded RAW to s3://{BUCKET}/{raw_key}")
+    
     timestamp = datetime.utcnow().strftime("%Y-%m-%d")
     folder = "processed" if is_valid_article(article) else "raw"
     key = f"{folder}/{source}/{timestamp}/{uuid.uuid4()}.json"
